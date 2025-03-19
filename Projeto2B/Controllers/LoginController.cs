478f9f3eecee3f7dc1e7868baa4046c24657db45
luiz <miguel342613@gmail.com>
2025-03-19 20:32:34 -0300

    Criando a estrutura de classes do projeto

---
+++ Projeto2B/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto2B.Controllers
+{
+    public class LoginController : Controller //Herança Controller está herdando a class LoginController 
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
